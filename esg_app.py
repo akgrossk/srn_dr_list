5f30fa9769d7f2d5e359b7ac6c7d3d00bfd41ce1
akgrossk <38951041+akgrossk@users.noreply.github.com>
2025-09-15 11:11:34 +0200

    Update esg_app.py

--- esg_app.py
+++ esg_app.py
@@ -1180,7 +1180,7 @@ def render_pillar(pillar: str, title: str, comparison: str, display_mode: str):
                 y_sort = [firm_series] + ([peers_series] if peers_series else [])
 
                 base = alt.Chart(cdf)
-                bars = base.mark_bar(stroke="#000", strokeWidth=1, strokeOpacity=0.9, strokeJoin="miter").encode(
+                bars = base.mark_bar().encode(
                     y=alt.Y("Series:N", title="", sort=y_sort),
                     x=alt.X("Value:Q", title="Number of Disclosure Requirements reported"),
                     color=alt.Color("StdCode:N",
@@ -1267,7 +1267,7 @@ def render_pillar(pillar: str, title: str, comparison: str, display_mode: str):
                 base = alt.Chart(cdf)
                 bars = (
                     base
-                    .mark_bar(stroke="#000", strokeWidth=1, strokeOpacity=0.9, strokeJoin="miter")  # default outline
+                    .mark_bar()  # default outline
                     .encode(
                         y=alt.Y("Series:N", title="", sort=y_sort),
                         x=alt.X("Value:Q", title="Number of Disclosure Requirements"),
